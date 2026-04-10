90cf1580f9fa282c0f1ae1545a24d30f8c2652c9
George-Okumu <gokumu12@gmail.com>
2026-04-10 18:08:00 +0300

    import marshmallow, used marshmallow serialization to serialize returned data

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pymysql
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask import Flask, jsonify
 from config import Config
-
 
 
 pymysql.install_as_MySQLdb()
@@ -11,13 +11,15 @@ app = Flask(__name__) # initializing the Flask application
 app.config.from_object(Config) # import db configurations
 
 db = SQLAlchemy(app) # initializing the SQLAlchemy object with the Flask app, which allows us to interact with the database using SQLAlchemy's ORM features.
-
+ma = Marshmallow(app)
 
 
 from models.user import User
 from models.profile import Profile
 from models.comment import Comment
 from models.blog import Blog
+from schemas.user_schema import UserSchema
+from schemas.blog_schema import BlogSchema
 
 def create_tables():
     with app.app_context():
@@ -29,22 +31,15 @@ create_tables()
     
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all() # Querying all User records from the database using SQLAlchemy's query interface. The result is a list of User objects.
-    return jsonify([{ "username": user.username, "email": user.email , "profile": {"bio":user.profile.bio}} for user in users])
+    users = User.query.all()
+    user_schema = UserSchema(many=True)
+    return jsonify(user_schema.dump(users))
 
 @app.route('/blogs', methods=['GET'])
 def get_blogs():
     blogs = Blog.query.all()
-    blogs_list = [
-        {
-            "id": blog.id,
-            "title": blog.title,
-            "comments": [
-                {"id": comment.id, "text": comment.content} for comment in blog.comments
-            ]
-        } for blog in blogs
-    ]
-    return jsonify(blogs_list)
+    blog_schema = BlogSchema(many=True)
+    return jsonify(blog_schema.dump(blogs))
 
 if __name__ == '__main__':
     app.run(debug=True)
